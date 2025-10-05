3ba219de7c19c79caf735aa2dc8011926b1cf9fe
Lina Khatsko <lina.khatsko@gmail.com>
2025-10-05 23:01:10 +0300

    fix App import and rename Modal css file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,6 +5,7 @@ import { fetchMovies } from "../../services/movieService";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
+import MovieModal from "../MovieModal/MovieModal";
 import type { Movie } from "../../types/movie";
 import styles from "./App.module.css";
 
